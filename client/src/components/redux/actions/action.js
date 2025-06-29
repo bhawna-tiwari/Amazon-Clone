e9b202c9730e40f463ffbefb2dfe3a69834c9614
bhawna-tiwari <bhawnatiwari7289.in>
2025-06-29 16:10:29 +0530

    fix: updated API URL to point to live backend

--- client/src/components/redux/actions/action.js
+++ client/src/components/redux/actions/action.js
@@ -1,17 +1,17 @@
 
-export const getProducts = ()=> async(dispatch)=>{
+export const getProducts = () => async (dispatch) => {
     try {
-        const data = await fetch("/getproducts",{
-                method:"GET",
-                headers:{
-                    "Content-Type":"application/json"
-                }
+        const data = await fetch("https://amazon-clone-project-u1p8.onrender.com/getproducts", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
         });
 
         const res = await data.json();
         console.log(res);
-        dispatch({type:"SUCCESS_GET_PRODUCTS",payload:res});
+        dispatch({ type: "SUCCESS_GET_PRODUCTS", payload: res });
     } catch (error) {
-        dispatch({type:"FAIL_GET_PRODUCTS",payload:error.response});
+        dispatch({ type: "FAIL_GET_PRODUCTS", payload: error.response });
     }
 }
