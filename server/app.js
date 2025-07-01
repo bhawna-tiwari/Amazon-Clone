2b43d7ba717ab9a3b61542e0d68202782fcbabda
bhawna-tiwari <bhawnatiwari7289.in>
2025-07-01 12:40:25 +0530

    fix: added vercel domain to CORS

--- server/app.js
+++ server/app.js
@@ -12,11 +12,24 @@ const router = require("./routes/router");
 
 app.use(express.json());
 app.use(cookieParser(""));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://amazon-clone-one-gamma.vercel.app/" 
+];
+
 app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-  }));
-app.use(router);
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 const PORT = process.env.PORT || 8006;
 app.listen(port,()=>{
